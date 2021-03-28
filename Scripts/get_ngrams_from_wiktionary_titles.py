fb0af667b68df09ab4bffa801a2ec9566d3cdf70
Mostafa <sr87@zips.uakron.edu>
2021-03-27 18:01:30 -0600

    Added 5gram to get_ngrams_from_wiktionary_titles.py

--- Scripts/get_ngrams_from_wiktionary_titles.py
+++ Scripts/get_ngrams_from_wiktionary_titles.py
@@ -100,17 +100,47 @@ def get_english_quadgrams_from_wiktioary_titles(json_file_out):
                     second = stemmer.stem(second)                
                     third = stemmer.stem(third)                
                     fourth = stemmer.stem(fourth)                
-                    if first in english_words and second in english_words and third in english_words: 
+                    if first in english_words and second in english_words and third in english_words and fourth in english_words: 
                         count += 1
                         print(title, count)
                         quadgrams.append(split_by_[0] + ' ' + split_by_[1] + ' ' + split_by_[2] + ' ' + split_by_[3])
 
     with open(json_file_out, "w") as outfile:  
-        json.dump(quadgrams, outfile)        
+        json.dump(quadgrams, outfile) 
+
+def get_english_pentagrams_from_wiktioary_titles(json_file_out):
+    titles_file = 'C:/myProjects/TDI/CapstoneProject/Wiktionary/enwiktionary-latest-all-titles'
+    titles_df = pd.read_csv(titles_file, delimiter='\t')
+    count = 0
+    pentagrams = []
+    for _, title in titles_df['page_title'].items():
+        split_by_ = str(title).split('_')
+        N_words = len(split_by_)
+        if N_words == 5: 
+            first = split_by_[0]
+            second = split_by_[1]
+            third = split_by_[2]
+            fourth = split_by_[3]
+            fifth = split_by_[4]
+            if len(first) > 0 and len(second) > 0 and len(third) > 0 and len(fourth) > 0 and len(fifth) > 0:
+                if first[0].islower() and second[0].islower() and third[0].islower() and fourth[0].islower() and fifth[0].islower():
+                    first = stemmer.stem(first)
+                    second = stemmer.stem(second)                
+                    third = stemmer.stem(third)                
+                    fourth = stemmer.stem(fourth)                
+                    fifth = stemmer.stem(fifth)                
+                    if first in english_words and second in english_words and third in english_words and fourth in english_words and fifth in english_words: 
+                        count += 1
+                        print(title, count)
+                        pentagrams.append(split_by_[0] + ' ' + split_by_[1] + ' ' + split_by_[2] + ' ' + split_by_[3] + ' ' + split_by_[4])
+
+    with open(json_file_out, "w") as outfile:  
+        json.dump(pentagrams, outfile)                
 
 if __name__ == '__main__':
 
-    get_english_unigrams_from_wiktioary_titles(json_file_out='C:/myProjects/SubtitlePro/Data/wiktionary_unigrams.json')
+    #get_english_unigrams_from_wiktioary_titles(json_file_out='C:/myProjects/SubtitlePro/Data/wiktionary_unigrams.json')
     #get_english_bigrams_from_wiktioary_titles(json_file_out='C:/myProjects/SubtitlePro/Data/wiktionary_bigrams.json')
     #get_english_trigrams_from_wiktioary_titles(json_file_out='C:/myProjects/SubtitlePro/Data/wiktionary_trigrams.json')
     #get_english_quadgrams_from_wiktioary_titles(json_file_out='C:/myProjects/SubtitlePro/Data/wiktionary_quadgrams.json')
+    get_english_pentagrams_from_wiktioary_titles(json_file_out='C:/myProjects/SubtitlePro/Data/wiktionary_pentagrams.json')
