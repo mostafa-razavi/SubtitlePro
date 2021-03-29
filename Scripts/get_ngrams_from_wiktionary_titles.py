6f11d81cd0684e213fbaa5ac70101bc17a9c135b
Mostafa <sr87@zips.uakron.edu>
2021-03-29 02:06:19 -0600

    Implemented get_ngrams_from_wiktionary_titles.p to fin ngrams from wiktionary titles and found n=8,9,10

--- Scripts/get_ngrams_from_wiktionary_titles.py
+++ Scripts/get_ngrams_from_wiktionary_titles.py
@@ -137,10 +137,103 @@ def get_english_pentagrams_from_wiktioary_titles(json_file_out):
     with open(json_file_out, "w") as outfile:  
         json.dump(pentagrams, outfile)                
 
+def get_english_hexagrams_from_wiktioary_titles(json_file_out):
+    titles_file = 'C:/myProjects/TDI/CapstoneProject/Wiktionary/enwiktionary-latest-all-titles'
+    titles_df = pd.read_csv(titles_file, delimiter='\t')
+    count = 0
+    hexagrams = []
+    for _, title in titles_df['page_title'].items():
+        split_by_ = str(title).split('_')
+        N_words = len(split_by_)
+        if N_words == 6: 
+            first = split_by_[0]
+            second = split_by_[1]
+            third = split_by_[2]
+            fourth = split_by_[3]
+            fifth = split_by_[4]
+            sixth = split_by_[5]
+            if len(first) > 0 and len(second) > 0 and len(third) > 0 and len(fourth) > 0 and len(fifth) > 0 and len(sixth) > 0:
+                if first[0].islower() and second[0].islower() and third[0].islower() and fourth[0].islower() and fifth[0].islower() and sixth[0].islower():
+                    first = stemmer.stem(first)
+                    second = stemmer.stem(second)                
+                    third = stemmer.stem(third)                
+                    fourth = stemmer.stem(fourth)                
+                    fifth = stemmer.stem(fifth)                
+                    sixth = stemmer.stem(sixth)                
+                    if first in english_words and second in english_words and third in english_words and fourth in english_words and fifth in english_words and sixth in english_words: 
+                        count += 1
+                        print(title, count)
+                        hexagrams.append(split_by_[0] + ' ' + split_by_[1] + ' ' + split_by_[2] + ' ' + split_by_[3] + ' ' + split_by_[4] + ' ' + split_by_[5])
+
+    with open(json_file_out, "w") as outfile:  
+        json.dump(hexagrams, outfile)
+
+def get_english_heptagrams_from_wiktioary_titles(json_file_out):
+    titles_file = 'C:/myProjects/TDI/CapstoneProject/Wiktionary/enwiktionary-latest-all-titles'
+    titles_df = pd.read_csv(titles_file, delimiter='\t')
+    count = 0
+    heptagrams = []
+    for _, title in titles_df['page_title'].items():
+        split_by_ = str(title).split('_')
+        N_words = len(split_by_)
+        if N_words == 7: 
+            first = split_by_[0]
+            second = split_by_[1]
+            third = split_by_[2]
+            fourth = split_by_[3]
+            fifth = split_by_[4]
+            sixth = split_by_[5]
+            seventh = split_by_[6]
+            if len(first) > 0 and len(second) > 0 and len(third) > 0 and len(fourth) > 0 and len(fifth) > 0 and len(sixth) > 0 and len(seventh) > 0:
+                if first[0].islower() and second[0].islower() and third[0].islower() and fourth[0].islower() and fifth[0].islower() and sixth[0].islower() and seventh[0].islower():
+                    first = stemmer.stem(first)
+                    second = stemmer.stem(second)                
+                    third = stemmer.stem(third)                
+                    fourth = stemmer.stem(fourth)                
+                    fifth = stemmer.stem(fifth)                
+                    sixth = stemmer.stem(sixth)                
+                    seventh = stemmer.stem(seventh)                
+                    if first in english_words and second in english_words and third in english_words and fourth in english_words and fifth in english_words and sixth in english_words and seventh in english_words: 
+                        count += 1
+                        print(title, count)
+                        heptagrams.append(split_by_[0] + ' ' + split_by_[1] + ' ' + split_by_[2] + ' ' + split_by_[3] + ' ' + split_by_[4] + ' ' + split_by_[5] + ' ' + split_by_[6])
+
+    with open(json_file_out, "w") as outfile:  
+        json.dump(heptagrams, outfile)
+
+def get_english_ngrams_from_wiktioary_titles(n, json_file_out):
+    titles_file = 'C:/myProjects/TDI/CapstoneProject/Wiktionary/enwiktionary-latest-all-titles'
+    titles_df = pd.read_csv(titles_file, delimiter='\t')
+    count = 0
+    ngrams = []
+    for _, title in titles_df['page_title'].items():
+        words_list = str(title).split('_')
+        N_words = len(words_list)
+        if N_words == n: 
+            for word in words_list:
+                if len(word) > 0 and word[0].islower() and stemmer.stem(word.lower()) in english_words:
+                    is_ngram_good = True
+                    pass
+                else:
+                    is_ngram_good = False
+                    break
+            if is_ngram_good == True:
+                count += 1
+                print(title, count)
+                ngrams.append(''.join(words_list))
+
+    with open(json_file_out, "w") as outfile:  
+        json.dump(ngrams, outfile)
+
 if __name__ == '__main__':
 
     #get_english_unigrams_from_wiktioary_titles(json_file_out='C:/myProjects/SubtitlePro/Data/wiktionary_unigrams.json')
     #get_english_bigrams_from_wiktioary_titles(json_file_out='C:/myProjects/SubtitlePro/Data/wiktionary_bigrams.json')
     #get_english_trigrams_from_wiktioary_titles(json_file_out='C:/myProjects/SubtitlePro/Data/wiktionary_trigrams.json')
     #get_english_quadgrams_from_wiktioary_titles(json_file_out='C:/myProjects/SubtitlePro/Data/wiktionary_quadgrams.json')
-    get_english_pentagrams_from_wiktioary_titles(json_file_out='C:/myProjects/SubtitlePro/Data/wiktionary_pentagrams.json')
+    #get_english_pentagrams_from_wiktioary_titles(json_file_out='C:/myProjects/SubtitlePro/Data/wiktionary_pentagrams.json')
+    #get_english_hexagrams_from_wiktioary_titles(json_file_out='C:/myProjects/SubtitlePro/Data/wiktionary_hexagrams.json')
+    #get_english_heptagrams_from_wiktioary_titles(json_file_out='C:/myProjects/SubtitlePro/Data/wiktionary_heptagrams.json')
+    #get_english_ngrams_from_wiktioary_titles(n=8, json_file_out='C:/myProjects/SubtitlePro/Data/wiktionary_octagrams.json')
+    #get_english_ngrams_from_wiktioary_titles(n=9, json_file_out='C:/myProjects/SubtitlePro/Data/wiktionary_nonagrams.json')
+    get_english_ngrams_from_wiktioary_titles(n=10, json_file_out='C:/myProjects/SubtitlePro/Data/wiktionary_decagrams.json')
